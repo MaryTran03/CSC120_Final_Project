3e1981e9a300b582f27358ef8bb76b6ab2d86569
Ada Li <ajli@smith.edu>
2024-11-21 13:58:56 -0500

    Updated Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Game {
-  public void checkWinStatus(){
-        if(){
+    private static final String save_file = "save_game.txt";
+    /**
+     * Method to check status for win.
+     */
+    public void checkWinStatus(){
+        if( ){
+            System.out.println("You win.");
+        } else if(){
+
+        }else{
+            
         }
     } 
     
     public static void pause(){
+        System.out.println("The game is paused. Do you want to save the progress?");
+        Scanner userInput = new Scanner(System.in);
+        String response = userInput.nextLine().toUpperCase();
+        if(response.equals("YES")){
+           try {
+                
+            } catch (Exception e) {
+ 
+            }
+        }else{
 
-    }  
+        }
+    }
+
+    public static void resume(){
+        System.out.println("The game continues.");
+    }
 }
