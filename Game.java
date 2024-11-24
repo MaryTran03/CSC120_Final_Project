3416f311f5d384bce271383524051bd97e3d8cdb
Adajli <ajli@smith.edu>
2024-11-24 11:51:32 -0500

    Modified code to avoid errors.

--- Game.java
+++ Game.java
@@ -1,14 +1,21 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
     private static final String SAVE_FILE = "save_game.dat";
     private int money;
     private int reputation;
+    private ArrayList<User> users;
+    private ArrayList<Mission> missions;
+
+    public Game(ArrayList<Mission> missions){
+        this.missions = missions;
+    }
     /**
      * Method to start the game.
      */
-    public static void startGame() {
+    public boolean startGame() {
         boolean started = false;
         System.out.println("Click on start or resume.");
         Scanner userInput = new Scanner(System.in);
@@ -18,8 +25,14 @@ public class Game {
         } else {
             System.out.println("Welcome to the game.");
         }
+        started = true;
+        userInput.close();
+        return started;
     }
 
+    public void addUsers(int reputation, int money){
+        users.add(new User(this.reputation,this.money));
+    }
     /**
      * Method to check status for win.
      */
@@ -34,24 +47,24 @@ public class Game {
     /**
      * Method to pause the game.
      */
-    public static void pause(){
+    public static boolean pause(){
         boolean pause = false;
         try {
+            pause = true;
             saveandStoreProgress();
-            System.in.read();
-        } catch(Error e) {
-            throw new RuntimeException("Can't pause the game.");
-         }
+        } catch(Exception e) {
+            throw new RuntimeException("Can't pause the game.", e);
+        }
+        return pause;
     }
 
     /**
      * Method to resume the game.
      */
     public static void resume(){
-        boolean resumed = false;
-        if(){
+        File saveFile = new File(SAVE_FILE);
+         if(saveFile.exists()){
             System.out.println("The game continues.");
-            resumed = true;
         }else{
             throw new RuntimeException("Can't resume the game.");
         }
@@ -60,20 +73,21 @@ public class Game {
     /**
      * Method to save and store progress of the game.
      */
-    public static boolean saveandStoreProgress(){
-        try(ObjectOutputstream out = new ObjectOutputstream(new FileOutputStream(SAVE_FILE))){
-             File saveFile = new File(SAVE_FILE);
-             return saveFile.exists();
-        } catch {
-             throw new RuntimeException("Can't save the game. Starting the game again.");
-        }
+    public static void saveandStoreProgress(){
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(SAVE_FILE))){
+            out.writeObject("Data Progress");
+        } catch (IOException e){
+            throw new RuntimeException("Can't save the game. Starting the game again.");
+        }  
+             
     }
+    
 
     /**
      * Method to end the game.
      */
     public static void endGame() {
-
+        System.out.println("The game is over.");
     }
 
 
