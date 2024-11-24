67ad485932a5e039f2fe5f0a55d18c456a2250c6
Nimco-Xuseen <158048534+Nimco-Xuseen@users.noreply.github.com>
2024-11-24 17:06:45 -0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,29 @@
+public class Player {
+    private String name;
+    private int reputation;   // Starts at 0
+    private double money;     // Starting amount of money
+   
+    public Player(String name, double money) {
+        this.name = name;
+        this.reputation = 0;  // Start reputation at 0
+        this.money = money;   // Starting money
+       
+    }
+
+    // Getters and Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getReputation() { return reputation; }
+    public void setReputation(int reputation) { this.reputation = reputation; }
+
+    public double getMoney() { return money; }
+    public void setMoney(double money) { this.money = money; }
+
+
+    public void printStatus() {
+        System.out.println("Name: " + name);
+        System.out.println("Reputation: " + reputation);
+        System.out.println("Money: " + money);
+    }
+}
